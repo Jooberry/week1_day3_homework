0b936f5fafb780a9a876a7ef681fc91b2efaf75b
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-03-08 18:35:10 +0000

    very nearly finished everything bar from one exercise

--- tdd_array_loop_hash_lab2.rb
+++ tdd_array_loop_hash_lab2.rb
@@ -1,6 +1,57 @@
-prices = [ 1.23, 6.98, 8.43, 2.45 ]
-costs = [ 4.23, 1.12, 0.52, 8.67 ]
-
-def add
-  puts prices.length + costs.length
+def add_array_lengths(array1, array2)
+  return array1.length() + array2.length()
 end
+
+
+def sum_array(array)
+  sum = 0
+  for number in array
+    sum += number
+  end
+  return sum
+end
+
+
+def find_item(array, item)
+  for house in array
+    if house == item
+    return true
+    end
+  end
+  return false
+end
+
+
+def get_first_key(teachers_wallets)
+  return teachers_wallets.keys()[0]
+end
+
+countries = {
+  uk: {
+    capital: 'London',
+    population: 60
+  },
+  france: {
+    capital: 'Paris',
+    population: 70
+  },
+  italy: {
+    capital: 'Rome',
+    population: 56
+  }
+}
+
+def array_of_capitals(array)
+  array.keys = []
+end
+
+
+
+
+
+
+
+
+
+
+
---
+++ tdd_array_loop_hash_lab_spec.rb
@@ -0,0 +1,56 @@
+require( 'minitest/autorun' )
+require( 'minitest/rg' )
+require_relative( 'tdd_array_loop_hash_lab2' )
+
+class Tdd_array_loop_hash_lab2 < MiniTest::Test
+
+def test_add_length_of_arrays
+      result = add_array_lengths( [ 1.23, 6.98, 8.43, 2.45 ], [ 4.23, 1.12, 0.52, 8.67 ] )
+      assert_equal( 8, result )
+end
+
+
+def test_sum_array
+  result = sum_array( [ 1,2,3,4,5 ] )
+  assert_equal( 15, result )
+end
+
+
+def test_find_item_true
+  result = find_item( [ 'Hufflepuff', 'Slytherin', 'Gryffindor', 'Ravenclaw' ], "Batman" )
+  assert_equal( true, result )
+end
+
+
+def test_get_first_key
+  wallets = {
+    'Kat' => 12,
+    'John'  => 10,
+    'Darren'  => 1356,
+    'Zsolt' => 1
+  }
+  result = get_first_key( wallets )
+  assert_equal( 'Kat', result )
+end
+
+def test_array_of_capitals
+  countries = {
+    uk: {
+      capital: 'London',
+      population: 60
+    },
+    france: {
+      capital: 'Paris',
+      population: 70
+    },
+    italy: {
+      capital: 'Rome',
+      population: 56
+    }
+  }
+  result = array_of_capitals( countries ) 
+  assert_equal( [ 'London', 'Paris', 'Rome'], result)
+end
+
+
+end
